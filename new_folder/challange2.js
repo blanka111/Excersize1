824e0dffb87a336a012acdbbeef1fa89a1edd555
blanka111 <blanka.github@gmail.com>
2024-04-28 16:39:01 +0200

    code for 1st and nd excersize

--- new_folder/challange2.js
+++ new_folder/challange2.js
@@ -52,5 +52,10 @@ for (let i = 0; i < vstup; i++)
     }
 }
 
+/*
+If your country's population is greater than 33 million, log a string like this to the console: "Portugal's population is 22 million below average" (the 22 is the average of 33 minus the country's population).
+After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+
+*/
 
 
