50cc417fbbd34c2003baf952800321bb9284f035
blanka111 <blanka.github@gmail.com>
2024-05-03 22:09:46 +0200

    code for 1st and nd excersize

--- word_frequency_counter.js
+++ word_frequency_counter.js
@@ -1,5 +1,31 @@
 const fs =require ('fs');
 
-fs.readFile('file.txt', 'utf8', (err, data) => {
+fs.readFile('file.txt', 'utf8', (err, data) => {   //vraci error nebo data//
+if (err) {
+    console.error(err);
+    return;
+}
 
+console.log(data);  // nactu data//
+    let words = data.replace('.',' ').;  //nahrazeni pouze u prvniho nalezeneho znaku //
+
+    let words=data.split(' ');  // oddeleni slov na zaklade toho, co je v zavorkach - mezera //
+
+    let wordsUnique =[];
+
+    words.forEach((el) => {
+            if(wordsUnique.includes(word.toLowerCase()))   // v danem poli je obsazeno toto slovo //
+            {
+            }
+            else {
+                wordsUnique.push(word.toLowerCase());
+            }
+
+    });
 })
+
+/*
+1. Nacist data ze souboru
+2.Rozdelit vetu na slova.
+3. Zjistit vyskyt slova ve vete.
+ */
